f1078397dbba3235ef9be858f18b48db88c326b8
SOURADEEP HAZRA <105676331+Souradeephazra123@users.noreply.github.com>
2025-03-23 19:41:01 +0530

    Updaetd all prices page

--- src/app/allprices/page.tsx
+++ src/app/allprices/page.tsx
@@ -48,8 +48,8 @@ export default function Home() {
   };
   console.log(prices);
   return (
-    <div>
-      <p>Prices</p>
+    <div className="p-7  globalbg globalbgtext bg-gradient-to-r from-[#0d142100] via-black to-[#0D1421] min-h-screen">
+      <p className=" text-xl font-bold">All Prices</p>
       {/* <div>{renderPrices()}</div> */}
 
       <div className="container mx-auto p-4">
@@ -65,7 +65,7 @@ export default function Home() {
           {Object.entries(prices).map(([key, value]) => (
             <div
               key={key}
-              className="bg-white rounded-lg shadow-md p-4" // Added styling
+              className="bg-gradient-to-br from-[#1A1A2E] to-[#16213E] text-white rounded-lg p-4  border border-blue-500/30 hover:scale-105 transition-transform duration-200 shadow-lg " // Added styling
             >
               <p className="text-lg font-medium">{key}</p>{" "}
               {/* Improved typography */}
